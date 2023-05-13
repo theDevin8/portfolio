806092a52f9488557d7af043ccb19b4f7d27ae0d
theDevin8 <devinstockton2003@gmail.com>
2023-05-12 23:09:28 -0500

    new changes

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 
-import React, { useEffect } from 'react';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
 import './App.css';
 import Layout from './componets/layout';
 
